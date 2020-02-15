c089ed1b4db657664346b119365bc141fc8eed0e
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2020-02-15 00:21:00 -0800

    heroku deployment

---
+++ client/build/precache-manifest.1a43599c33f933522d94ee5a637f4458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b07fb8be4470245bd6f881de64a4445",
+    "url": "/static/media/books.3b07fb8b.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c1db615cbba9bb32b22e",
+    "url": "/static/js/main.1bd69c95.chunk.js"
+  },
+  {
+    "revision": "0a745084e99d9a08a3c8",
+    "url": "/static/js/2.3224a69a.chunk.js"
+  },
+  {
+    "revision": "c1db615cbba9bb32b22e",
+    "url": "/static/css/main.235deba6.chunk.css"
+  },
+  {
+    "revision": "7d2d024d813560b645e84ded59dc12e0",
+    "url": "/index.html"
+  }
+];
